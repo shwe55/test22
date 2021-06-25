7cf3781240aee1f1761e6388d3d6520b57c4d434
shweta chitnis <shweta.chitnis@qset.in>
2021-06-25 12:06:02 +0530

    Add all my files

---
+++ ios/SocialNetwork/SwiftHeader.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftHeader.swift
+//  SocialNetwork
+//
+//  Created by Florian Marcu on 9/9/20.
+//  Copyright © 2020 Instamobile Code SRL. All rights reserved.
+//
+
+import UIKit
+
+class SwiftHeader: NSObject {
+
+}
